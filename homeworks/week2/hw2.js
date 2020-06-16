95311719e1b2dd19963f330fedb930c0fa018a53
huiming <hero19931012@gmail.com>
2020-06-16 22:02:14 +0800

    hw2-6 finished

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,14 @@
 function capitalize(str) {
-  
+  const codeOfHead = str.charCodeAt(0);
+  let result = '';
+  if (codeOfHead >= 97 && codeOfHead <= 122) {
+    result = String.fromCharCode(codeOfHead - 32);
+    // eslint-disable-next-line no-plusplus
+    for (let i = 1; i < str.length; i++) {
+      result += str[i];
+    }
+  }
+  return result || str;
 }
 
 console.log(capitalize('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,23 @@
+/* eslint-disable no-plusplus */
 function join(arr, concatStr) {
-  
+  let result = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    result += concatStr;
+    result += arr[i];
+  }
+  return result;
 }
 
 function repeat(str, times) {
-  
+  let result = '';
+  for (let i = 1; i <= times; i++) {
+    result += str;
+  }
+  return result;
 }
 
-console.log(join(['a'], '!'));
+console.log(join([1, 2, 3], '!'));
+console.log(join(['a', 'b', 'c'], '!'));
+console.log(join(['a', 1, 'b', 2, 'c', 3], ','));
 console.log(repeat('a', 5));
+console.log(repeat('yoyo', 2));
