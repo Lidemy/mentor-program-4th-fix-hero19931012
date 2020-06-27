fa82dcc8311745127e84ca497d3baaedab561a91
huiming <hero19931012@gmail.com>
2020-06-27 10:39:04 +0800

    添加挑戰題註解

--- homeworks/week3/挑戰題 (走迷宮).js
+++ homeworks/week3/挑戰題 (走迷宮).js
@@ -17,38 +17,45 @@ function enqueue(queue, x, y) {
 function BFS(queue, nodes, H, W) {
   let exit = false;
   let steps = -1;
+  // 宣告 steps 記錄走了幾步，設為 -1 是為了修正從 queue 中移除 0, 0 產生的 1 步
   while (exit !== true) {
     const temp = [];
+    // 為了確實將所有可能的 '下一步' 先走完，需要一個暫存佇列
     while (queue.length !== 0) {
       temp.push(queue.shift());
+      // 把 queue 的內容移到 temp，同時把 queue 清空
     }
     while (temp.length !== 0) {
       const node = temp.shift();
+      // 從 temp 中取出第 1 個坐標
       // eslint-disable-next-line no-param-reassign
       nodes[node].v = 1;
+      // 將這個點標為已造訪
 
       const [x, y] = nodes[node].p;
+      // 取出坐標
       if (x === H - 1 && y === W - 1) {
         exit = true;
-        break;
       }
+      // 如果坐標等於出口就把 exit 設為 true 表示已經找到，之後便結束迴圈並印出 steps
       // eslint-disable-next-line quotes
       if (nodes[`${x - 1}, ${y}`] !== undefined && nodes[`${x - 1}, ${y}`].v === 0) { enqueue(queue, x - 1, y); }
       if (nodes[`${x + 1}, ${y}`] !== undefined && nodes[`${x + 1}, ${y}`].v === 0) { enqueue(queue, x + 1, y); }
       if (nodes[`${x}, ${y - 1}`] !== undefined && nodes[`${x}, ${y - 1}`].v === 0) { enqueue(queue, x, y - 1); }
       if (nodes[`${x}, ${y + 1}`] !== undefined && nodes[`${x}, ${y + 1}`].v === 0) { enqueue(queue, x, y + 1); }
+      // 判斷當前坐標的 4 個方位是不是可以走的點以及是否尚未造訪，若是則加入 queue
     }
-    // eslint-disable-next-line no-plusplus
-    steps++;
+    steps += 1;
+    // 當把 temp 清空時才算走了 1 步
   }
   console.log(steps);
 }
 
 
-function solve(input) { // 取得 lines
+function solve(input) { // 取得 input
   const [H, W] = input[0].split(' ').map(Number);
-  const map = [...lines.slice(1)];
-
+  // 取得寬高
+  const map = [...input.slice(1)];
   const nodes = {};
   for (let i = 0; i < H; i += 1) {
     for (let j = 0; j < W; j += 1) {
@@ -60,8 +67,12 @@ function solve(input) { // 取得 lines
       }
     }
   }
+  // 將所有可走的點用物件記錄起來
+  // 每個點帶 2 個屬性，p 表示坐標，v 表示造訪狀態，0: 未造訪，1: 已造訪
+
   // eslint-disable-next-line quotes
   const queue = [`0, 0`];
+  // 設定起點為 0, 0
   BFS(queue, nodes, H, W, -1);
 }
 
--- homeworks/week3/超級挑戰題 (貪婪的小偷 Part 2).js
+++ homeworks/week3/超級挑戰題 (貪婪的小偷 Part 2).js
@@ -13,25 +13,39 @@ function knapSack(N, capacity, item) {
   if (!knapSack.answer) {
     knapSack.answer = {};
   }
+  // 建立快取
+
   if (knapSack.answer[`${N}, ${capacity}`] !== undefined) {
     return knapSack.answer[`${N}, ${capacity}`];
   }
+  // 檢查快取中是否已經有計算值，有就直接回傳
 
   if (N === 0 || capacity === 0) {
     return 0;
   }
+  // 如果物品拿完或背包已經沒有容量，等於無法增加任何價值，直接回傳 0
+
   const tempN = knapSack(N - 1, capacity, item);
+  // 宣告不拿第 N 項物品的價值，物品編號 - 1，背包容量不變
+
   if (item[N - 1][0] > capacity) {
     return tempN;
   }
+  // 如果這項物品的重量大於背包容量，總價值等於不拿這項物品的剩餘價值，等於 tempN
+
   const tempY = knapSack(N - 1, capacity - item[N - 1][0], item) + item[N - 1][1];
+  // 若拿取這項物品，總價值等於該項物品的價值加上剩下的背包容量可放入的最大價值
+  // 故物品編號 -1，背包容量 - 當前物品的重量
+
   knapSack.answer[`${N}, ${capacity}`] = tempN > tempY ? tempN : tempY;
   return knapSack.answer[`${N}, ${capacity}`];
+  // 計算拿或不拿哪個總價值較高並回傳
 }
 
 function solve(input) {
   const [N, capacity] = input[0].split(' ').map(Number);
   const item = input.slice(1).map(x => x.split(' ').map(Number));
+  // 建立物品重量與價值的二維陣列
   console.log(knapSack(N, capacity, item));
 }
 
