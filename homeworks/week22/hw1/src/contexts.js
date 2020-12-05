2e5ad97651efcaae182725ad9f2386bf4961425c
huiming <hero19931012@gmail.com>
2020-12-05 16:37:41 +0800

    week 22 hw1, hw2

---
+++ homeworks/week22/hw1/src/contexts.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-unresolved */
+import { createContext } from 'react';
+
+// 利用 createContext 新增 AuthContext
+export const AuthContext = createContext(null);
